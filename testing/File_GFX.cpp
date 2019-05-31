c6b66cc7e1ed1b9e782e5fbceab3a2d9a7268777
Marco Cattani <m.cattani@gmail.com>
2019-05-31 22:23:37 +0100

    fixed color conversion

--- testing/File_GFX.cpp
+++ testing/File_GFX.cpp
@@ -10,11 +10,15 @@ File_GFX::File_GFX(int width, int height, char *filename) : Adafruit_GFX(width, 
 
 void File_GFX::drawPixel(int16_t x, int16_t y, uint16_t color)
 {
-	ezd_set_pixel(hDib, x, _height - y, color);
+	int r = (((color >> 11) & 31) / 31.0) * 255;
+	int g = (((color >> 5) & 63) / 63.0) * 255;
+	int b = ((color & 31) / 31.0) * 255;
+	int c = (r << 16) | (g << 8) | (b);
+
+	ezd_set_pixel(hDib, x, _height - y, c);
 }
 
 void File_GFX::flush(void)
 {
 	ezd_save(hDib, filename);
 }
-
