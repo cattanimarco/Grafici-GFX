3e884244a1590c6fb664dbb5b6858ed19ed2247c
Marco Cattani <m.cattani@gmail.com>
2019-11-14 23:45:22 +0000

    refactoring colors

---
+++ src/Colors.h
@@ -0,0 +1,14 @@
+#ifndef GRAFICI_COLORS_H
+#define GRAFICI_COLORS_H
+#include "Display.h"
+
+extern ColorPalette csBright;
+extern ColorPalette csBw;
+extern ColorPalette csCmyk;
+extern ColorPalette csFrance;
+extern ColorPalette csHeat;
+extern ColorPalette csNeon;
+extern ColorPalette csParula;
+extern ColorPalette csRainbow;
+
+#endif //GRAFICI_COLORS_H
--- src/modules/bw.h
+++
@@ -1,9 +0,0 @@
-#include "../Display.h"
-
-#ifndef GRAFICI_GFX_BW_H
-#define GRAFICI_GFX_BW_H
-
-extern Color cBw[];
-extern ColorPalette csBw;
-
-#endif //GRAFICI_GFX_BW_H
