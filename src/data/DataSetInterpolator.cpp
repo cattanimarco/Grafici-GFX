12a7f697955234ec019fe9e11679bb512aa20da5
Marco Cattani <m.cattani@gmail.com>
2019-12-12 16:22:45 +0000

    wip

--- src/data/DataSetInterpolator.cpp
+++ src/data/DataSetInterpolator.cpp
@@ -1,62 +1,68 @@
 #include "DataSetInterpolator.h"
 
 DataSetInterpolator::DataSetInterpolator(DataSet &dataSetX, DataSet &dataSetY, int interpolationSteps)
-    : dataSetInX(&dataSetX)
-    , dataSetInY(&dataSetY)
-// , dataSetX(generatorX, interpolationSteps)
-// , dataSetY(generatorY, interpolationSteps)
+    : dataSetX(&dataSetX)
+    , dataSetY(&dataSetY)
 {
-	dataSetX = new DataSetParametric(&generatorX, interpolationSteps);
 	setLength(interpolationSteps);
 	refresh();
 }
 
 DataCoordinate DataSetInterpolator::getDataCoordinate(int index) const
 {
-	return generatorY(index);
-}
-
-void DataSetInterpolator::refresh(void)
-{
-	dataSetInX->refresh();
-	dataSetInY->refresh();
-}
-
-DataCoordinate DataSetInterpolator::generatorX(int index)
-{
-	if (index < length())
-	{
-		return DataCoordinate{ (1.0 * index) / (length() - 1) };
-	}
-	else
+	if (DatasetSelection::none == selectedDataset)
 	{
 		return DataCoordinate{ 0 };
 	}
-}
 
-DataCoordinate DataSetInterpolator::generatorY(int index)
-{
 	if (index < length())
 	{
 		int bin = 0;
-		DataCoordinate x = generatorX(index);
+		DataCoordinate x = (1.0 * index) / (length() - 1);
+
+		if (DatasetSelection::x == selectedDataset)
+		{
+			return x;
+		}
 
 		// check that we are in the correct bin
-		while (x > dataSetInX->getDataCoordinate(bin + 1))
+		while (x > dataSetX->getDataCoordinate(bin + 1))
 		{
 			bin++;
-			if (bin == dataSetInX->length())
+			if (bin == dataSetX->length())
 			{
 				bin--;
 				break;
 			}
 		}
 
-		float fractBetween = (x - dataSetInX->getDataCoordinate(bin)) / (dataSetInX->getDataCoordinate(bin + 1) - dataSetX->getDataCoordinate(bin));
-		return DataCoordinate{ dataSetInY->getDataCoordinate(bin + 1) * fractBetween + dataSetInY->getDataCoordinate(bin) * (1.0 - fractBetween) };
+		float fractBetween = (x - dataSetX->getDataCoordinate(bin)) / (dataSetX->getDataCoordinate(bin + 1) - dataSetX->getDataCoordinate(bin));
+		return DataCoordinate{ dataSetY->getDataCoordinate(bin + 1) * fractBetween + dataSetY->getDataCoordinate(bin) * (1.0 - fractBetween) };
 	}
 	else
 	{
 		return DataCoordinate{ 0 };
 	}
+}
+
+void DataSetInterpolator::refresh()
+{
+	dataSetX->refresh();
+	dataSetY->refresh();
+}
+
+DataSetInterpolator DataSetInterpolator::interpolatedX() const
+{
+	/* return a copy */
+	DataSetInterpolator result = *this;
+	result.selectedDataset = DatasetSelection::x;
+	return result;
+}
+
+DataSetInterpolator DataSetInterpolator::interpolatedY() const
+{
+		/* return a copy */
+	DataSetInterpolator result = *this;
+	result.selectedDataset = DatasetSelection::y;
+	return result;
 }
--- src/data/DataSetInterpolator.h
+++ src/data/DataSetInterpolator.h
@@ -12,21 +12,20 @@ class DataSetInterpolator : public DataSet
 	DataCoordinate getDataCoordinate(int index) const override;
 	void refresh() override;
 
-	// FloatLimits dataLimitsX();
-	// FloatLimits setDataLimitsX(FloatLimits limits);
-	// FloatLimits dataLimitsY();
-	// FloatLimits setDataLimitsY(FloatLimits limits);
-
-	DataSetParametric dataSetX;
-	DataSetParametric dataSetY;
+	DataSetInterpolator interpolatedX() const;
+	DataSetInterpolator interpolatedY() const;
 
   private:
-	DataCoordinate generatorX(int index);
-	DataCoordinate generatorY(int index);
-
-	FloatLimits limits{ 0, 0 };
-	DataSet *dataSetInX{ nullptr };
-	DataSet *dataSetInY{ nullptr };
+	enum class DatasetSelection
+	{
+		none,
+		x,
+		y,
+	};
+	
+	DatasetSelection selectedDataset{ DatasetSelection::none };
+	DataSet *dataSetX{ nullptr };
+	DataSet *dataSetY{ nullptr };
 };
 
 #endif /* GRAFICI_DATASET_INTERPOLATE_H */
