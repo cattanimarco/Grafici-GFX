da4a49e87a4fc1b84a601f65c52169b3dfc8983a
Marco Cattani <marcocattani@Marcos-MacBook-Pro.local>
2020-05-15 11:30:43 +0100

    make first arduino example to work

--- examples/simple_plot/simple_plot.ino
+++ examples/simple_plot/simple_plot.ino
@@ -1,26 +1,24 @@
 #include "Adafruit_GFX.h" // Hardware-specific library
-#include <MCUFRIEND_kbv.h>
+#include "Adafruit_ILI9341.h"
 
 #include "Grafici.h"
 
-#include "Display.h"
+Adafruit_ILI9341 gfx = Adafruit_ILI9341(10, 9);
 
-MCUFRIEND_kbv gfx;
-
-DataSource::Float dataset;
-float dataArray[5] = { 1, 0, 2, 1, 2 };
+constexpr size_t num_elem = 5;
+float array[num_elem] = { 1, 0, 2, 1, 2 };
+DataSource::Linear x(num_elem);
+DataSource::Array<float> y(array, num_elem);
+DataSource::Constant c(num_elem, 1);
 
 void setup(void)
 {
-	gfx.begin(0x7575);
+	gfx.begin();
 	gfx.setRotation(1);
 
-	grafici.begin(gfx);
-
-	dataset.begin(dataArray, 1, 5); // load array of 5 floats
-
-	grafici.clear();
-	grafici.plot(Line, dataset); // plot dataSource using a simple line
+	grafici.begin(gfx, Colors::blackAndWhite);
+  grafici.clear();
+  grafici.plot(line, x, y, c);
 }
 
 void loop(void)
