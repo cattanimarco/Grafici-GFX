d90191fb0688cb322abf47ac30e9341162327f96
Marco Cattani <marcocattani@Marcos-MacBook-Pro.local>
2020-05-26 23:50:42 +0100

    added linear interpolation example

---
+++ examples/linear_interpolation/linear_interpolation.ino
@@ -0,0 +1,30 @@
+#include "Adafruit_GFX.h" // Hardware-specific library
+#include "Adafruit_ILI9341.h"
+
+#include "Grafici.h"
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(10, 9);
+
+constexpr size_t array_elements = 5;
+constexpr size_t interpolated_elements = 50;
+
+float dataArray[array_elements] = { 1, 0, 2, 1, 2 };
+float colorArray[array_elements] = { 0, 1, 2, 3, 4 };
+DataSource::Linear x(array_elements);
+DataSource::Array<float> y(dataArray, array_elements);
+DataSource::Array<float> c(colorArray, array_elements);
+DataSet::LinearInterpolator dataLinear{ x, y, c, c, interpolated_elements };
+
+void setup(void)
+{
+	tft.begin();
+	tft.setRotation(1);
+
+	grafici.begin(tft, Colors::csHeat);
+	grafici.clear();
+	grafici.plot(line, dataLinear);
+}
+
+void loop(void)
+{
+}
